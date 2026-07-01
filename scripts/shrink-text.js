c23ef6cbe57135d289cfd44c5db31693aa5c94e4
amorphea <alex@westminstersoftware.com>
2026-07-01 16:13:34 +0930

    Remove most text-fit.js info now that the code has mostly been written from scratch anyway

--- scripts/shrink-text.js
+++ scripts/shrink-text.js
@@ -1,31 +1,4 @@
 class ShrinkText {
-	// Based upon text-fit.js by STRML
-	//
-	// textFit v2.3.1
-	// Previously known as jQuery.textFit
-	// 11/2014 by STRML (strml.github.com)
-	// MIT License
-	//
-	// The MIT License (Expat License):
-	//
-	// Copyright (c) 2014 STRML (strml.github.com)
-	//
-	// Permission is hereby granted, free of charge, to any person obtaining a copy of this
-	// software and associated documentation files (the "Software"), to deal in the Software
-	// without restriction, including without limitation the rights to use, copy, modify,
-	// merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-	// permit persons to whom the Software is furnished to do so, subject to the following
-	// conditions:
-	//
-	// The above copyright notice and this permission notice shall be included in all copies
-	// or substantial portions of the Software.
-	//
-	// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-	// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-	// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-	// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-	// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-	// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 	static shrinkText(element, width, maxHeight) {
 		if (!isElement(element)) {
 			return false;
@@ -35,7 +8,7 @@ class ShrinkText {
 
 		// First, shrink the font size by halves until the text fits within the max height
 		for (let i = 0; true; i++) {
-			let rect = element.getBoundingClientRect();
+			let rect = element.getBoundingClientRect(); // Based on text-fit.js by STRML (https://github.com/STRML/textFit/tree/master)
 			if (rect.height <= maxHeight) break;
 
 			ShrinkText.setFontSize(element, ShrinkText.getFontSize(element) / 2);
@@ -53,7 +26,7 @@ class ShrinkText {
 			let mean = (minSize + maxSize) / 2;
 			ShrinkText.setFontSize(mean);
 
-			let rect = element.getBoundingClientRect();
+			let rect = element.getBoundingClientRect(); // Based on text-fit.js by STRML (https://github.com/STRML/textFit/tree/master)
 			if (rect.height <= maxHeight) {
 				minSize = mean;
 			} else {
@@ -61,57 +34,6 @@ class ShrinkText {
 			}
 		}
 		ShrinkText.setFontSize(minSize);
-
-
-
-		
-		var innerSpan, originalHeight, originalHTML, originalWidth;
-		var low, mid, high;
-
-		// Get element data.
-		originalHTML = el.innerHTML;
-		originalWidth = innerWidth(el);
-		originalHeight = innerHeight(el);
-
-		// Add textFitted span inside this container.
-		innerSpan = document.createElement('span');
-		innerSpan.className = 'textFitted';
-		// Inline block ensure it takes on the size of its contents, even if they are enclosed
-		// in other tags like <p>
-		innerSpan.style['display'] = 'inline-block';
-		innerSpan.innerHTML = originalHTML;
-		el.innerHTML = '';
-		el.appendChild(innerSpan);
-
-		// Prepare & set alignment
-		if (settings.alignHoriz) {
-			el.style['text-align'] = 'center';
-			innerSpan.style['text-align'] = 'center';
-		}
-
-		low = settings.minFontSize;
-		high = settings.maxFontSize;
-
-		// Binary search for highest best fit
-		var size = low;
-		while (low <= high) {
-			mid = (high + low) >> 1;
-			innerSpan.style.fontSize = mid + 'px';
-			var innerSpanBoundingClientRect = innerSpan.getBoundingClientRect();
-			if (
-				innerSpanBoundingClientRect.width <= originalWidth 
-				&& (settings.widthOnly || innerSpanBoundingClientRect.height <= originalHeight)
-			) {
-				size = mid;
-				low = mid + 1;
-			} else {
-				high = mid - 1;
-			}
-			// await injection point
-		}
-		// found, updating font if differs:
-		if( innerSpan.style.fontSize != size + 'px' ) innerSpan.style.fontSize = size + 'px';
-
 	}
 
 	static getFontSize(element) {
